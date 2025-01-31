537486c10a97fdf29ef7e3a49f90ae4ff43eafe4
22r01a0561 <138597453+rishik-08@users.noreply.github.com>
2025-01-31 10:13:54 +0530

    commit

---
+++ lib/constants/todo_examples.dart
@@ -0,0 +1,52 @@
+const List<String> todoExamples =
+[
+  'Buy groceries',
+  'Finish homework',
+  'Go for a run',
+  'Call mom',
+  'Pay bills',
+  'Clean the house',
+  'Read a book',
+  'Schedule doctor\'s appointment',
+  'Walk the dog',
+  'Write a blog post',
+  'Prepare dinner',
+  'Organize closet',
+  'Start a new project',
+  'Attend meeting at work',
+  'Do laundry',
+  'Visit the dentist',
+  'Plan weekend trip',
+  'Send thank-you cards',
+  'Practice playing guitar',
+  'Watch a movie',
+  'Update resume',
+  'Water the plants',
+  'Learn a new language',
+  'Volunteer at local shelter',
+  'Fix broken chair',
+  'Complete project proposal by 3 PM'
+      'Schedule a meeting with the marketing team'
+      'Reply to client emails'
+      'Buy groceries after work'
+      'Drop off dry cleaning on the way home'
+      'Pay utility bills by the end of the week'
+      'Create a budget for the next six months'
+      'Research and plan a dream vacation'
+      'Start a blog or a personal project'
+      'Buy groceries after work'
+      'Drop off dry cleaning on the way home'
+      'Pay utility bills by the end of the week'
+      'Go for a 30-minute jog in the morning'
+      'Prepare a healthy lunch for tomorrow'
+      'Schedule a check-up with the dentist'
+      'Read two chapters of a book before bedtime'
+      'Enroll in a language course online'
+      'Practice playing the guitar for at least 20 minutes'
+      'Declutter the living room'
+      'Fix the leaky faucet in the kitchen'
+      'Set up a cleaning schedule for the week'
+      'Plan a weekend outing with friends'
+      'Watch the latest episode of your favorite TV show'
+      'Attend a local event or meetup'
+];
